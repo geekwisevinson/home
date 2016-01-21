f30d97e53fde4721a29ee699198d0e1381bcc3b2
vinferno <vinsonfernandez@gmail.com>
2016-01-21 13:55:22 -0800

    i hate typos

--- vinsons_api.js
+++ vinsons_api.js
@@ -1,10 +1,8 @@
-{
-  "home_folder":"http://geekwisevinson.github.io/home/",
-  "auto_run":[
+home_folder = "http://geekwisevinson.github.io/home/";
+auto_run=[
     "alert('hi')",
     "alert('thanks')"
-  ],
-  "color_schemes":{
+];
+color_schemes={
     "beautiful_girl":[[[12,20,33],[336,75,30],[335,60,46],[29,49,83],[27,26,93]]]
-  }
-}
+};
