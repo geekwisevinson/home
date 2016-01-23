7fcc4fdd7edcde89e868921eefd0fd1f2e22ae5f
geekwisevinson <vinsonfernandez@gmail.com>
2016-01-22 23:25:22 -0800

    color

--- vinsons_defaults.js
+++ vinsons_defaults.js
@@ -6,23 +6,27 @@ auto_run=[
 
 
 function click_handler(e) {
+    e.preventDefault();
     e = e || window.event;
-    if ("which" in e)  // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
-        chr_moz_right= e.which == 3;
-    else if ("button" in e)  // IE, Opera
-        ie_right = e.button == 2;
+    var left= 0,
+        right=0;
 
-    if (chr_moz_right==true | ie_right==true){
-        alert("right")
+    if ("which" in e)  // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
+        right= e.which == 3;
+    else if ("button" in e)  // IE, Opera
+        right = e.button == 2;
+
+
+    if ("which" in e)  // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
+        left= e.which == 2;
+    else if ("button" in e)  // IE, Opera
+        left = e.button == 1;
+
+    if (right == 1){
+        alert('right');
     }
-
-    if ("which" in e)  // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
-        chr_moz_left= e.which == 2;
-    else if ("button" in e)  // IE, Opera
-        ie_left = e.button == 1;
-
-    if (chr_moz_right==true | ie_right==true){
-        alert("right")
+    if (left == 1){
+        alert('left');
     }
 
 }
