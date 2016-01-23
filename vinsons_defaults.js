6c090dffcffa6198fd201df6e43e5b6328e80e80
geekwisevinson <vinsonfernandez@gmail.com>
2016-01-22 22:57:10 -0800

    color

--- vinsons_defaults.js
+++ vinsons_defaults.js
@@ -4,20 +4,15 @@ auto_run=[
     "alert('thanks')"
 ];
 
-var left, right;
-left = mie ? 1 : 0;
-right = 2;
 
 function click_handler(e) {
-    var isRightMB;
     e = e || window.event;
-
     if ("which" in e)  // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
         chr_moz_right= e.which == 3;
     else if ("button" in e)  // IE, Opera
         ie_right = e.button == 2;
 
-    if (chr_moz_right==true|| ie_right==true){
+    if (chr_moz_right==true | ie_right==true){
         alert("right")
     }
 
