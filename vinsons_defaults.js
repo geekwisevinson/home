bde9674b55b21a490d12f2a93138b84904c38eee
geekwisevinson <vinsonfernandez@gmail.com>
2016-01-23 00:05:53 -0800

    color

--- vinsons_defaults.js
+++ vinsons_defaults.js
@@ -4,19 +4,4 @@ auto_run=[
     "alert('thanks')"
 ];
 
-function mouse_check(event) {
-    event.preventDefault();
-    switch (event.which) {
-        case 1:
-            alert('Left Mouse button pressed.');
-            break;
-        case 2:
-            alert('Middle Mouse button pressed.');
-            break;
-        case 3:
-            alert('Right Mouse button pressed.');
-            break;
-        default:
-            alert('You have a strange Mouse!');
-    }
-}
+
