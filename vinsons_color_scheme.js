2e6d9c7d9ed7e5889086d32ada0f5df8a1013dbd
geekwisevinson <vinsonfernandez@gmail.com>
2016-01-22 20:57:41 -0800

    color

--- vinsons_color_scheme.js
+++ vinsons_color_scheme.js
@@ -1,13 +1,8 @@
-color_schemes={
-    "beautiful_girl":[[12,15,23],[12,20,33],[336,75,30],[335,60,46],[29,49,83],[27,26,93]]
-};
-
+cs_path=['garden','ariana','jlaw','school','shire'];
 function hsl(ai){
     return 'hsl('+cs[ai][0]+","+cs[ai][1]+"%,"+cs[ai][2]+"%)";
 }
-
 function customs (){
-
         custom=document.getElementsByClassName('custom0');
         for (var i=0;i<custom.length;i++){
             custom[i].style.backgroundColor=cs[0];
@@ -39,9 +34,7 @@ function customs (){
             custom[i].style.color=cs[2];
         }
 }
-
 function populate_schemes(){
-
     color_schemer = document.getElementById('color_schemer');
     for (var i=0;i<cs_path.length;i++){
         cs_option =document.createElement('option');
@@ -52,16 +45,12 @@ function populate_schemes(){
     color_schemer.addEventListener('change',function(){
         load_scheme('../../../vinsons_txt_schemes/'+color_schemer.value+'.txt')
     })
-
 }
-
 function color_down(){
     if(color_schemer.length-1==color_schemer.selectedIndex){
         color_schemer.selectedIndex=0;
-
     }else{
         color_schemer.selectedIndex+=1;
     }
     load_scheme('../../../vinsons_txt_schemes/'+color_schemer.value+'.txt')
 }
-cs_path=['garden','ariana','jlaw','school'];
