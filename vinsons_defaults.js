8d70ee1ca810c3e4948882048d1e43bd74cd4a59
geekwisevinson <vinsonfernandez@gmail.com>
2016-01-26 12:59:15 -0800

    pics

--- vinsons_defaults.js
+++ vinsons_defaults.js
@@ -6,5 +6,6 @@ auto_run=[
 
 function bgpic(){
     rn= Math.floor(Math.random()*52);
-    document.body.style.backgroundImage="url(/home/img/pic0"+rn+".jpg)"
+    document.body.style.backgroundImage="url(/home/img/pic0"+rn+".jpg)";
+    document.body.style.backgroundSize= 'cover';
 }
